a63f836df150a248165c666daa12928b11fb8b09
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-10-27 20:56:05 +0600

    Project re-started

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7EbeKc4pxMTBaYKC1T4DY70fJAVDMkOI",
+  authDomain: "edu-lab-9a7f1.firebaseapp.com",
+  projectId: "edu-lab-9a7f1",
+  storageBucket: "edu-lab-9a7f1.appspot.com",
+  messagingSenderId: "269332195922",
+  appId: "1:269332195922:web:7b37ebaaacb41b6bdc4d8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
